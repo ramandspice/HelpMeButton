d021517efc7eab32a28461cf82524c090719ba00
ramandspice <ramandspice@gmail.com>
2015-04-15 11:44:54 -0400

    Add method name to LoginRequest.

--- app/src/main/java/com/helme/helpmebutton/rest/requests/LoginRequest.java
+++ app/src/main/java/com/helme/helpmebutton/rest/requests/LoginRequest.java
@@ -20,6 +20,7 @@ public class LoginRequest extends BaseRequest {
         }
     }
 
+    //Test Commit
     private static final String GRANT_TYPE = "password";
     private static final String METHOD_NAME = "logIn";
 
